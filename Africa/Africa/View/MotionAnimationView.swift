f9dadf0202daa8078130952a18dd4b33d109658e
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2021-07-23 08:16:42 +0200

    created and added backround animation to gallery

---
+++ Africa/Africa/View/MotionAnimationView.swift
@@ -0,0 +1,67 @@
+//
+//  MotionAnimationView.swift
+//  Africa
+//
+//  Created by Zdenko Čepan on 22/07/2021.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    // MARK: Properties
+    @State private var randomCircle = Int.random(in: 12...16)
+    @State private var isAnimating: Bool = false
+    
+    // MARK: Functions
+    func randomCoordinate(max: CGFloat) -> CGFloat {
+        return CGFloat.random(in: 0...max)
+    }
+    
+    func randomSize() -> CGFloat {
+        return CGFloat.random(in: 10...300)
+    }
+    
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
+    
+    func randomSpeed() -> Double {
+        return Double.random(in: 0.025...1)
+    }
+    
+    func randomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
+    
+    // MARK: Body
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(0...randomCircle, id: \.self) { item in
+                    Circle()
+                        .foregroundColor(.gray)
+                        .opacity(0.15)
+                        .scaleEffect(isAnimating ? randomScale() : 1)
+                        .frame(width: randomSize(), height: randomSize(), alignment: .center)
+                        .position(x: randomCoordinate(max: geometry.size.width),
+                                  y: randomCoordinate(max: geometry.size.height))
+                        .animation(Animation.interpolatingSpring(stiffness: 0.5, damping: 0.5)
+                                    .repeatForever()
+                                    .speed(randomSpeed())
+                                    .delay(randomDelay())
+                        )
+                        .onAppear(perform: {
+                            isAnimating = true
+                        })
+                } // LOOP
+            } // ZSTACK
+            .drawingGroup()
+        } // GEOMETRY
+    }
+}
+
+struct MotionAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MotionAnimationView()
+    }
+}
